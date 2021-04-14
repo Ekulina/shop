94d7a4479151ef1d7d41828d35d6bc848e0f2caf
Your Name <you@example.com>
2021-04-14 14:16:28 +0300

    cart

--- products.php
+++ products.php
@@ -4,23 +4,65 @@ return  [
     '1' => [
         
         'id' => 1,
-        'name' => 'Product1',
+        'name' => 'Lill1',
         'price' => 12.50,
         'image' => 'https://via.placeholder.com/150',
     ],
     '2' =>[
             
         'id' => 2,
-        'name' => 'Product2',
+        'name' => 'Lill2',
         'price' => 10.00,
         'image' => 'https://via.placeholder.com/150',
     ],
     '3' =>[
             
-            'id' => 3,
-            'name' => 'Product3',
-            'price' => 14.50,
-            'image' => 'https://via.placeholder.com/150',
+        'id' => 3,
+        'name' => 'Lill3',
+        'price' => 14.50,
+        'image' => 'https://via.placeholder.com/150',
     ],
-        
-    ];
+    '4' =>[
+            
+        'id' => 3,
+        'name' => 'Lill4',
+        'price' => 4.50,
+        'image' => 'https://via.placeholder.com/150',
+    ],
+    '5' =>[
+            
+        'id' => 3,
+        'name' => 'Lill5',
+        'price' => 22.50,
+        'image' => 'https://via.placeholder.com/150',
+    ],
+    '6' =>[
+            
+        'id' => 3,
+        'name' => 'Lill6',
+        'price' => 28.50,
+        'image' => 'https://via.placeholder.com/150',
+    ],
+    '7' =>[
+            
+        'id' => 3,
+        'name' => 'Lill7',
+        'price' => 30.00,
+        'image' => 'https://via.placeholder.com/150',
+    ],
+    '8' =>[
+            
+        'id' => 3,
+        'name' => 'Lill8',
+        'price' => 1.50,
+        'image' => 'https://via.placeholder.com/150',
+    ],
+    '9' =>[
+            
+        'id' => 3,
+        'name' => 'Lill9',
+        'price' => 2.00,
+        'image' => 'https://via.placeholder.com/150',
+    ]
+      
+];
